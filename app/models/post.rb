5f39016f492c67741c8c2ab99425f2c5048da976
gautamsawhney <gsawhney21@gmail.com>
2015-04-01 01:10:38 +0530

    Almost over

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ActiveRecord::Base
+  attr_accessible :body, :title,:image, :image_remote_url
+   attr_reader :image_remote_url
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  									:url => "/system/:class/:attachment/:id/:style_:basename.:extension",
+                    :path => ":rails_root/public/system/:class/:attachment/:id/:style_:basename.:extension"
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+
+  validates_presence_of :body, :title
+
+  def image_remote_url=(url_value)
+  self.image = URI.parse(url_value) unless url_value.blank?
+  super
+end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '43153774c886bf0fc756b42ca2c286e1a74e56a601dbdbdef17d660d5b3d543a7a8cfa20d06ba9a993b471a3f4e1c568ecf97b2d2d128dc3dee8bedb991876ec'
